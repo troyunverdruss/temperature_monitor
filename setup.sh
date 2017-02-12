88b0e611db91710f118f2dd6f77eb040ce3b5ce2
Troy Unverdruss <troy@unverdruss.net>
2017-02-12 14:27:14 -0800

    adding digitemp init command

--- setup.sh
+++ setup.sh
@@ -2,11 +2,15 @@
 /etc/init.d/ssh start
 apt-get update
 apt-get upgrade -y
-apt-get install ruby ruby-dev digitemp libmysqlclient-dev
+apt-get install ruby ruby-dev digitemp libmysqlclient-dev -y
+
 wget https://sqlite.org/snapshot/sqlite-snapshot-201702071351.tar.gz
 tar xzf sqlite-snapshot-201702071351.tar.gz
 cd sqlite-snapshot-201702071351/
 CFLAGS="-Os -DSQLITE_ENABLE_COLUMN_METADATA=1" ./configure
 make install
+cd ..
 
 gem install sqlite3 mysql
+
+digitemp_DS9097U  -s /dev/ttyUSB0 -i
