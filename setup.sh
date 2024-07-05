14e186bb205256f91d89a205fb390675379c9055
Troy Unverdruss <troy@unverdruss.net>
2024-07-05 15:13:57 -0700

    Updated to use postgres, connect directly instead of intermediate storage

--- setup.sh
+++ setup.sh
@@ -2,15 +2,8 @@
 /etc/init.d/ssh start
 apt-get update
 apt-get upgrade -y
-apt-get install ruby ruby-dev digitemp libmysqlclient-dev -y
+apt-get install ruby ruby-dev digitemp libpq-dev -y
 
-wget https://sqlite.org/snapshot/sqlite-snapshot-201702071351.tar.gz
-tar xzf sqlite-snapshot-201702071351.tar.gz
-cd sqlite-snapshot-201702071351/
-CFLAGS="-Os -DSQLITE_ENABLE_COLUMN_METADATA=1" ./configure
-make install
-cd ..
-
-gem install sqlite3 mysql
+gem install pg
 
 digitemp_DS9097U  -s /dev/ttyUSB0 -i
--- temperature_monitor.rb
+++ temperature_monitor.rb
@@ -1,74 +1,39 @@
 #!/usr/bin/env ruby
 
-require 'sqlite3'
-require 'optparse'
 require 'fileutils'
 require 'yaml'
-require 'mysql'
+require 'pg'
 require 'pathname'
 
 script_path = File.expand_path File.dirname(__FILE__)
 
-DATABASE_NAME = script_path.to_s + '/temperature.db'
+config = YAML::load_file script_path + '/config.yaml'
+c = config['db']
 
-options = {}
+reading = `digitemp_DS9097U -a -o "%R:%.2F" -q`
+reading.split(/\n/).each do |line|
+  sensor, temp = line.split(':')
+  puts 'reading: ' + sensor + temp
 
-OptionParser.new do |opts|
-  opts.banner = 'Usage: ./temperature_monitor.rb'
-
-  opts.on '-r', '--read', 'Read sensors' do |o|
-    options[:read] = true
-  end
-
-  opts.on '-u', '--upload', 'Upload data' do |o|
-    options[:upload] = true
-  end
-
-  opts.on '-c', '--clean', 'Clean local DB' do |o|
-    options[:clean] = true
-  end
-end.parse!
-
-if File.exist? DATABASE_NAME
-  db = SQLite3::Database.open DATABASE_NAME
-else
-  db = SQLite3::Database.new DATABASE_NAME
-  db.execute <<-SQL
-  CREATE TABLE data (
-    sensor_id VARCHAR(255),
-    epoch_timestamp INT,
-    temp_reading REAL,
-    uploaded INT
-  );
-  SQL
-end
-
-
-if options[:read]
-  reading=`digitemp_DS9097U  -a -o "%R:%.2F" -q`
-  reading.split(/\n/).each do |line|
-    sensor, temp = line.split(':')
-    puts 'reading: ' + sensor + temp
-    db.execute 'INSERT INTO data VALUES (?, ?, ?, ?)', [sensor, Time.now.to_i, temp, 0]
-  end
-end
-
-if options[:upload]
-  config = YAML::load_file script_path + '/config.yaml'
-  c = config['db']
   begin
-    connection = Mysql.connect(c['host'], c['user'], c['pass'], c['name'], c['port'])
+    connection = PG.connect(
+      {
+        user: c['user'],
+        password: c['pass'],
+        dbname: c['name'],
+        host: c['host'],
+        port: c['port'],
+      }
+    )
 
-    db.execute('SELECT rowid, sensor_id, epoch_timestamp, temp_reading FROM data WHERE uploaded = 0') do |row|
-      prepared_statement = connection.prepare 'INSERT INTO data (sensor_id, epoch_timestamp, temp_reading) VALUES (?, ?, ?)'
-      prepared_statement.execute row[1], row[2], row[3]
-      db.execute 'UPDATE data SET uploaded = ? WHERE rowid = ?', [1, row[0]]
-    end
+    connection.exec(
+      "INSERT INTO readings (sensor_id, temp_reading) VALUES ($1, $2)",
+      [sensor, temp]
+    )
+
   ensure
-    connection.close if connection
+    connection.finish
   end
 end
 
-if options[:clean]
-  db.execute 'DELETE FROM data WHERE uploaded = ?', 1
-end
+
